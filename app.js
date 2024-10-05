4819f973487aab5ee4dab88af7fff92978c90f33
amitdutta <duttaamit356@gmail.com>
2024-10-05 21:57:05 +0530

    api folder structure

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+require('dotenv').config();
+const authRouter = require('./route/authRoute')
+
+const app = express();
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Api is running'
+    })
+})
+const nodeModulesPath = process.env.NODE_MODULES_PATH;
+console.log(`Node Modules Path: ${nodeModulesPath}`);
+
+app.use('/api/v1/auth', authRouter)
+
+
+app.listen(3000,()=>{
+    console.log('server rinning in port 3000')
+})
---
+++ controller/authController.js
@@ -0,0 +1,7 @@
+const register = (req, res, next) => {
+    res.json({
+        message: 'register api route'
+    })
+}
+
+module.exports = { register }
